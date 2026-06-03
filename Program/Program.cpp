24f203cd1c8e17b60e6fb332be95e1e51ae02cbb
leejake9335-cmd <leejake9335@gmail.com>
2026-06-03 19:45:34 +0900

    (2026.06.03) 선형 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,8 +1,68 @@
 ﻿#include "stdafx.h"
+#include <vector>
+#include <list>
+
 
 
 int main()
 {
+#pragma region 선형 컨테이너
+    // 데이터를 선형으로 저장하며, 특별한 제약이나
+    // 규칙이 없는 컨테이너입니다.
+
+#pragma region vector container
+
+    // vector<int> vector;
+    // 
+	// vector.reserve(8);
+    // 
+    // vector.push_back(10);
+    // 
+	// vector.push_back(20);
+    // 
+    // vector.push_back(30);
+    // 
+    // vector.push_back(40);
+    // 
+    // vector.push_back(50);
+    // 
+    // vector.pop_back();
+    // 
+	// for (int i = 0; i < vector.size(); i++)
+	// {
+	// 	cout << vector[i] << endl;
+	// }
+    // 
+	// vector.clear();
+    // 
+	// cout << "vector size : " << vector.size() << endl;
+    // 
+    // cout << "vector capacity : " << vector.capacity() << endl;
+
+#pragma endregion
+
+#pragma region list container
+    
+    list<int> list;
+
+	list.push_back(20);
+
+	list.push_back(30);
+
+	list.push_back(10); 
+
+    list.pop_front();
+
+	list.push_front(1);
+
+	list.remove(20);
+
+    
+
+#pragma endregion
+
+
+#pragma endregion
 
 
     return 0;
