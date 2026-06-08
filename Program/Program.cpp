bdb1a834b8aeaa2789516bbf3f6bdea5a40a4b0a
pse260 <seeunbag450@gmail.com>
2026-06-08 19:47:09 +0900

    (2026.06.08) 반복자

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,43 +1,8 @@
 ﻿#include "stdafx.h"
-#include <set>
-
-
 
 
 int main()
 {
-#pragma region 연관 컨테이너
-    // 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너 입니다.
-    
-#pragma region set
-	// std::set<const char*> set;
-	// 
-	// set.insert("Project Moon");
-	// set.insert("Limbus Company");
-	// set.insert("Library of Ruina");
-	// set.insert("Lobotomy Corporation");
-	// 
-	// for (const auto & element : set)
-	// {
-	// 	cout << element << endl;
-	// }
-	// 
-	// cout << "set empty : " << set.empty() << endl;
-	// 
-	// set.clear();
-	// 
-	// cout << "set size : " << set.size() << endl;
-	// cout << "set max size : " << set.max_size() << endl;
-
-#pragma endregion
-
-
-
-
-
-
-#pragma endregion
-
 
     return 0;
 }
